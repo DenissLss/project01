5c71f3acd6c287bf58b9e9886adb9e823e7dac09
deniss <surge271@gmail.com>
2024-06-12 19:29:49 +0300

    d

--- TESTS.NR7/JS/script.js
+++ TESTS.NR7/JS/script.js
@@ -2,10 +2,10 @@ function myFunction(p1, p2) {
     return p1 * p2;
   }
   
-  let result = myFunction(4, 3);
+  let result = myFunction(55, 3);
   document.getElementById("demo").innerHTML = result;
   
   
-  result = myFunction(5, 6);
+  result = myFunction(12, 12);
   document.getElementById("demo1").innerHTML = result;
   
