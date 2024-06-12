35f7f8703721f97942c9b9901e417bf9484ad334
deniss <surge271@gmail.com>
2024-06-12 19:48:35 +0300

    done

--- TESTS.NR7/JS/script.js
+++ TESTS.NR7/JS/script.js
@@ -1,11 +1,6 @@
-function myFunction(p1, p2) {
-    return p1 * p2;
-  }
-  
-  let result = myFunction(55, 3);
-  document.getElementById("demo").innerHTML = result;
-  
-  
-  result = myFunction(12, 12);
-  document.getElementById("demo1").innerHTML = result;
-  
+function toCelsius(f) {
+  return (5/9) * (f-32);
+}
+
+let value = toCelsius;
+document.getElementById("demo").innerHTML = value;
